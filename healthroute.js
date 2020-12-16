3c6edd29fd0ccfadb8295c510b096c87d7f8cc3c
Marco Wettstein <maw@panter.ch>
2020-12-16 17:05:57 +0100

    feat: show build info if available

--- healthroute.js
+++ healthroute.js
@@ -1,6 +1,43 @@
-import { WebApp } from 'meteor/webapp'
+import { WebApp } from "meteor/webapp";
+import { Meteor } from "meteor/meteor";
+
+import { readFileSync } from "fs";
+
+const BUILD_INFO_FILE = `${process.cwd()}/__build_info.json`;
+
+let buildInfo = undefined;
+
+export const getBuildInfo = () => {
+  try {
+    if (typeof buildInfo === "undefined") {
+      if (Meteor.isDevelopment) {
+        buildInfo = {
+          id: "dev-local",
+          commit: "dev-local-commit",
+          tag: "dev-local-tag",
+          time: new Date().toISOString(),
+        };
+      } else {
+        try {
+          const data = readFileSync(BUILD_INFO_FILE, "utf-8");
+          buildInfo = data ? JSON.parse(data) : null;
+        } catch (e) {
+          //
+        }
+      }
+    }
+
+    console.log({ buildInfo });
+
+    return buildInfo;
+  } catch (err) {
+    console.error("loading the build info failed", err);
+    return {};
+  }
+};
 
 WebApp.connectHandlers.use(`/__health`, (req, res) => {
-  res.writeHead(200)
-  res.end('healthy')
-})
+  res.writeHead(200);
+  const buildInfo = getBuildInfo();
+  res.end(JSON.stringify({ buildInfo }));
+});
--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'panter:healthroute',
-  version: '0.1.0',
+  name: "panter:healthroute",
+  version: "0.2.0",
   // Brief, one-line summary of the package.
-  summary: 'simple package that adds a route __health',
+  summary: "simple package that adds a route __health",
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: "",
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
-})
+  documentation: "README.md",
+});
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.0.2')
-  api.use('ecmascript')
-  api.use('webapp')
-  api.mainModule('healthroute.js', ['server'])
-})
+Package.onUse(function (api) {
+  api.versionsFrom("1.8.0.2");
+  api.use("ecmascript");
+  api.use("webapp");
+  api.mainModule("healthroute.js", ["server"]);
+});
 
-Package.onTest(function(api) {
-  api.use('ecmascript')
-  api.use('tinytest')
-  api.use('panter:healthroute')
-})
+Package.onTest(function (api) {
+  api.use("ecmascript");
+  api.use("tinytest");
+  api.use("panter:healthroute");
+});
