dd7ef1c96d53b488e7fe720674b9aab225fab073
Marco Wettstein <maw@panter.ch>
2021-02-16 19:53:42 +0100

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "panter:healthroute",
-  version: "0.2.1",
+  version: "0.3.0",
   // Brief, one-line summary of the package.
   summary: "simple package that adds a route __health",
   // URL to the Git repository containing the source code for this package.
